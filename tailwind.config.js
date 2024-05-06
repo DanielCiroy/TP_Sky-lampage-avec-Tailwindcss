5ad522a1bd7e217477276fe54c835564b66851e2
Daniel ciroy <142291477+DanielCiroy@users.noreply.github.com>
2024-05-06 13:19:10 +0200

    la baniere

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      couleurbg: '#00040f',
+      couleur2: '#00f6ff',
+      couleur3: 'rgba(225,225,225, 0.7)',
+      couleur4: 'rgba(9,151,124,0.1)'
+    },
+    fonts: {
+      ff: '["poppins","sans-serif"]'
+    }
   },
   plugins: [],
 }
